cefef38e345929d2e5df2d54b5b1bd3439cbb896
s10046602 <mitchellcrainred@gmail.com>
2023-09-19 10:34:42 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+class BoxLayout(BoxLayout):
+    pass
+
+class MainWidget(Widget):
+    pass
+
+class WidgetLayoutApp(App):
+    pass
+
+WidgetLayoutApp().run()
